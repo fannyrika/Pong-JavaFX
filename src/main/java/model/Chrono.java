b0af80fbafecb2df5e875ef22dc30a045ee59066
Romain <romain.houard18@gmail.com>
2022-10-20 15:28:01 +0200

    Modification class chrono

--- src/main/java/model/Chrono.java
+++ src/main/java/model/Chrono.java
@@ -1,14 +1,12 @@
 package model;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 
 public class Chrono {
 
-  public IntegerProperty mm  = new SimpleIntegerProperty();
-  public IntegerProperty ss  = new SimpleIntegerProperty();
-  public IntegerProperty th  = new SimpleIntegerProperty();
-  public IntegerProperty hd  = new SimpleIntegerProperty();
-  public IntegerProperty cpt = new SimpleIntegerProperty();
+  public int mm  = 0;
+  public int ss  = 0;
+  public int th  = 0;
+  public int hd  = 0;
+  public int cpt = 0;
 
 
   
@@ -20,22 +18,21 @@ public class Chrono {
 
   public void reset() {
 
-    mm.set(0);
-    ss.set(0);
-    th.set(0);
-    hd.set(0);
-    cpt.set(0);
+    mm=0;
+    ss=0;
+    th=0;
+    hd=0;
+    cpt=0;
   }
 
   public void update() {
-    cpt.set(cpt.get() + 2);
-    hd.set(cpt.get() % 10);
-    th.set((cpt.get() / 10) % 10);
-    ss.set(((cpt.get() / 100) % 60));
-    mm.set((cpt.get() / 6000) % 60);
+    cpt=cpt + 2;
+    hd=cpt % 10;
+    th=(cpt / 10) % 10;
+    ss=(((cpt/ 100) % 60));
+    mm=((cpt / 6000) % 60);
 
 
 
   }
-
 }
