3e5793fd8bb320888c035088af59831d5f0a16ff
Lili <lilia.pinto.07@gmail.com>
2022-11-04 21:13:58 +0100

    class music

---
+++ src/main/java/model/Music.java
@@ -0,0 +1,25 @@
+package model;
+
+
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+public class Music {
+
+	public Media media;
+    public MediaPlayer mP;
+    public MediaView mV;
+
+    public Music(){
+    	media = new Media(new File("pongmusic.mp3").toURI().toString());
+    	mP = new MediaPlayer(media);
+        mP.setCycleCount(mP.INDEFINITE);
+        mP.setAutoPlay(true);
+
+        mV = new MediaView(mP);
+
+    }
+
+}
