4244bd5ca2d2c8963478081e293775b2df0f8ea7
pintol <lilia.pinto.07@gmail.com>
2022-11-06 12:12:30 +0100

    changement musique

--- src/main/java/model/Music.java
+++ src/main/java/model/Music.java
@@ -1,4 +1,4 @@
-/*package model;
+package model;
 
 
 import java.io.File;
@@ -6,20 +6,20 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 
+
 public class Music {
 
 	public Media media;
     public MediaPlayer mP;
     public MediaView mV;
-
-    public Music(){
-    	media = new Media(new File("pongmusic.mp3").toURI().toString());
+    
+	public Music(){
+    	media = new Media(new File("fallen_leaves.mp3").toURI().toString());
     	mP = new MediaPlayer(media);
         mP.setCycleCount(mP.INDEFINITE);
         mP.setAutoPlay(true);
 
         mV = new MediaView(mP);
-
     }
-
-}*/
+	
+}
